c0f5cb0a6db8d6c739ddcfaf042c8e96fed1a293
johntech-dev <johnayomide50@gmail.com>
2023-09-07 16:41:42 +0100

    'm'

--- src/Component/Creative/Creative.jsx
+++ src/Component/Creative/Creative.jsx
@@ -4,7 +4,7 @@ const Creative = () => {
   return (
     <div id='Creative' className='mt-[30px]'>
       <div className='flex flex-row justify-center mt-[70px] text-center'>
-        <h2 className='font-outfit font-normal text-center text-[30px] mt-4'>My Creative <span className='text-purple-600'>Portfolio</span></h2>  
+        <h2 className='font-outfit font-normal text-center text-[20px] mt-4'>My Creative <span className='text-purple-600'>Portfolio</span></h2>  
         <img src='robo.png' className='animate-bounce h-[80px] mb-20' />                                
       </div>
     </div>
