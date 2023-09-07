bc7619dcf5c635e2740a839850a6816fee10c3f6
johntech-dev <johnayomide50@gmail.com>
2023-09-07 16:07:54 +0100

    'm'

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Nav from './Component/Nav/Nav';
 import About from './Component/About/About';
 import Skill from './Component/Skill/Skill';
+import Creative from './Component/Creative/Creative';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Skill />
+      <Creative />
     </div>
   );
 }
---
+++ src/Component/Creative/Creative.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Creative = () => {
+  return (
+    <div id='Creative' className='mt-[30px]'>
+      <div className='flex flex-row justify-center mt-[70px]'>
+        <h2 className='font-outfit font-normal text-center text-[40px] mt-4'>My Creative <span className='text-purple-600'>Portfolio</span></h2>  
+        <img src='robo.png' className='animate-bounce h-[80px] mb-20' />                                
+      </div>
+    </div>
+  )
+}
+
+export default Creative
