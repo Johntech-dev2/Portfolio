a966fc7cb28cbb922d35c1da6a82f371805fc89d
johntech-dev <johnayomide50@gmail.com>
2023-09-07 15:31:59 +0100

    'm'

--- src/Component/Skill/Skill.jsx
+++ src/Component/Skill/Skill.jsx
@@ -7,7 +7,7 @@ const Skill = () => {
          <img src='skill.png' className='animate-bounce h-[80px] mb-4 ' />
          <h3 className='font-outfit font-light text-[30px] mt-2'>Skills & <span className='text-purple-600'>Expertise</span></h3>
        </div>
-       <div className='flex flex-col md:flex-row  justify-evenly px-8 md:px-0 mt-8 '>
+       <div className='flex flex-col md:flex-row  justify-evenly px-8 md:px-0 mt-7 '>
        <div className='grid grid-cols-4 md:grid-cols-4 gap-8  items-center'>
        <div className='w-[60px] transition ease-in-out delay-100 hover:scale-125 bg-purple-50p-3 rounded-full' >
      <img src='react.png'
