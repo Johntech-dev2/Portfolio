34ee4292cfc18ca36c3ec72a151f587185d2f31c
johntech-dev <johnayomide50@gmail.com>
2023-09-08 14:14:04 +0100

    'j'

--- src/Component/Ccf/Contact.jsx
+++ src/Component/Ccf/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
             <label className='text-gray-400 '>Message</label>
             <textarea type="text" rows={7} placeholder='Type your message here' required
              className='border-[1.5px] px-2 hover:border-purple-500 border-purple-300 rounded-md'  />
-           <button className='transition-all ease-in-out hover:scale-110 bg-purple-500 flex flex-row items-center justify-center text-[14px]  py-1 mt-5 text-white  rounded-md'>
+           <button className='transition-all ease-in-out hover:scale-110 bg-purple-500 flex flex-row items-center justify-center text-[14px]  py-1 mt-5 text-white  rounded-md mb-4'>
            Submit
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-2 w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
